0a56265ba0446fd6d27d1d557d98a31df2739ea0
Giovanni Panasiti <giova.panasiti@gmail.com>
2018-10-23 15:40:37 +0200

    indentation in the model

--- models/portale.go
+++ models/portale.go
@@ -8,7 +8,7 @@ import (
 // Represents a movie, we uses bson keyword to tell the mgo driver how to name
 // the properties in mongodb document
 type LogPortale struct {
-	ID      	bson.ObjectId 			`bson:"_id" json:"id"`
-	Payload    	interface{}      			`bson:"payload" json:"payload"`
+	ID      	bson.ObjectId 	`bson:"_id" json:"id"`
+	Payload    	interface{}     `bson:"payload" json:"payload"`
 	CreatedAt	time.Time		`bson:"createdAt" json:"createdAt"`
 }
